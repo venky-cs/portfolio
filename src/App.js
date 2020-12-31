cb9c3d566b8486ea185154c46126266d50c4e398
venkatesh <75250713+venky-cs@users.noreply.github.com>
2020-12-31 19:49:03 +0530

    'images-added'

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header image="" name="Billy Person" role="Front-end developer" email="billy@example.com"
+      <Header image="https://biomedicalodyssey.blogs.hopkinsmedicine.org/files/2020/02/programming-code-window-GettyImages-1124838925_640.jpg" name="Billy Person" role="Front-end developer" email="billy@example.com"
         phone="(+603)546 624 342"/>
 
         <Main />
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,7 +7,7 @@ function Blog() {
             <h3 className="title">
                 How to organize your CSS
             </h3>
-            <img src="" alt=""/>
+                <img src="https://content.techgig.com/thumb/msid-75672064,width-860,resizemode-4/Bill-Gates-recommends-these-online-free-resources-to-learn-coding.jpg?66999" alt=""/>
             </div>
             <p>In this article I tell the story about Proin eu justo sit amet lacus bibendum tincidunt.Vivamus non volutpat nisl, a luctus mi. </p>
 
