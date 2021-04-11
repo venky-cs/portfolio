bd1b1f86c3f3e2c6446e06480bd7135e31025eca
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-04-11 21:49:51 +0530

    'final-commit'

--- src/components/Hobbie.js
+++ src/components/Hobbie.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Hobbie({image,hobbie,exp}) {
     return (
         <div className="hobbie">
-            <img src={image} alt=""/>
+            <img src={image} alt={hobbie+"img"}/>
             <h4>{hobbie}</h4>
             <p>{exp}</p>
         </div>
